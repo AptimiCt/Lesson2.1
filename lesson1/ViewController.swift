61cccf06a07c79023a687061f24acbe18ba917ea
AptimiCt <AptimiCt11@gmail.com>
2019-07-23 22:12:02 +0300

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Александр Востриков on 23/07/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
